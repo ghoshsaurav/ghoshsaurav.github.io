2bf6682a3139c773036c03cd1fecdd96ab4cce8c
ghoshsaurav <71978046+ghoshsaurav@users.noreply.github.com>
2026-05-12 21:44:13 +0000

    Deploying to gh-pages from @ ghoshsaurav/ghoshsaurav.github.io@eeef387a0815f05cd1a4fa5a53e11a75b04579b4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
-        },{id: "nav-research",
+  },{id: "nav-research",
           title: "Research",
           description: "Refereed, non-refereed publications, in-preparation work, and highlight videos in reverse chronological order.",
           section: "Navigation",
