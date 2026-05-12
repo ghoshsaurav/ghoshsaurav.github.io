bcb6677ff15508150ee865464adafafd38a367dd
ghoshsaurav <71978046+ghoshsaurav@users.noreply.github.com>
2026-05-12 21:20:11 +0000

    Deploying to gh-pages from @ ghoshsaurav/ghoshsaurav.github.io@ee659144f50e02c430f35b20102eff6261e791df 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,9 +23,9 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
+        },{id: "nav-selected-projects",
+          title: "Selected Projects",
+          description: "academic, research, and software works.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
